338d3019739595ae300b2b3c481bc2e740705deb
Craig R Webster <craig@barkingiguana.com>
2010-02-03 16:41:43 +0000

    Initial commit: a skeleton project.

---
+++ spec/irc/spec_client.js
@@ -0,0 +1,12 @@
+var IRC    = require(path.join(process.cwd(), 'lib', 'irc', 'client')),
+    Client = IRC.Client;
+
+describe("An IRC client", function() {
+  beforeEach(function() {
+    
+  });
+
+  it("should ...", function() {
+    // ...
+  });
+})
